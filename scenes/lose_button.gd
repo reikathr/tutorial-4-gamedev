0fa4ed8209717378b18206d2dfcebb004f0d5a74
meteoreinger <reikathr@gmail.com>
2025-03-07 20:58:41 +0700

    add restart after dying

--- scenes/lose_button.gd
+++ scenes/lose_button.gd
@@ -1,4 +1,6 @@
 extends TextureButton
 
+@export var sceneName: String = "Level1"
+
 func _on_pressed():
-	get_tree().reload_current_scene()
+	get_tree().change_scene_to_file(str("res://scenes/" + sceneName + ".tscn"))
