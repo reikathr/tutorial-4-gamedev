59bd96c9b9bb5258b6b0e36528b5c992684c5b85
meteoreinger <reikathr@gmail.com>
2025-05-16 12:20:50 +0700

    finish tutorial 8

--- scripts/Spawner.gd
+++ scripts/Spawner.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var obstacle : PackedScene
+@export var spawnTime : int = 2
 
 func _ready():
 	repeat()
@@ -13,8 +14,11 @@ func spawn():
 	spawn_pos.x = spawn_pos.x + randf_range(-1000, 1000)
 
 	spawned.global_position = spawn_pos
+	
+	if spawned is RigidBody2D:
+		spawned.gravity_scale = 0.3
 
 func repeat():
 	spawn()
-	await get_tree().create_timer(1).timeout
+	await get_tree().create_timer(spawnTime).timeout
 	repeat()
